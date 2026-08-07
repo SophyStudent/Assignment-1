aa4b1f88e95d8f2e2950caa57a28f1d19862c6db
SophyStudent <233377597+SophyStudent@users.noreply.github.com>
2026-08-07 23:29:55 +0100

    add

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});   
+//in-memory database
+const students = [];
+function generateid() {
+    return students.length + 1
+};
+
+// create a student
+app.post("/student", (req, res) => {
+const studentData = req.body;
+const student = {
+    ...studentData,
+    id: generateid()
+};
+students.push(student);
+res.json(student);
+});
+
+// get all students
+app.get("/all-students", (req, res) => {
+    res.json(students);
+});
+
